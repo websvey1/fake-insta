2f523ce48167d867895eada35444728db7466f75
websvey1 <websvey1@gmail.com>
2019-04-12 06:25:01 +0000

    04_upload multiple image_set

--- posts/models.py
+++ posts/models.py
@@ -1,11 +1,26 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 # Create your models here.
 
 class Post(models.Model):
     content = models.TextField()
-    image = models.ImageField(blank=True)
-    
+    # image = ProcessedImageField(
+    #             upload_to='posts/images',					
+    #             processors=[ResizeToFill(600, 600)],		
+    #             format='JPEG',								
+    #             options={'quality': 90},					
+    # 				)
     
     def __str__(self):
         return self.content
+        
+class Image(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)   # Post가 부모객체, 
+    file = ProcessedImageField(
+                upload_to='posts/images',					
+                processors=[ResizeToFill(600, 600)],		
+                format='JPEG',								
+                options={'quality': 90},					
+    )
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_list_or_404,get_object_or_404
-from .models import Post
-from .form import PostForm
+from .models import Post, Image
+from .form import PostForm, ImageForm
 # Create your views here.
 
 def list(request):
@@ -10,16 +10,25 @@ def list(request):
 
 def create(request):                        # 글 작성을 클릭하면?
     if request.method == "POST":             # 요청이(어디서부터들어온) POST방식이면(form.html에서 정의한대로 자기 자신의링크로 POST방식으로 보내는방식)
-        post_form = PostForm(request.POST, request.FILES)       # PostForm을 form 변수에 담음 , 이미지는 FILES에 있음
+        post_form = PostForm(request.POST)       # PostForm을 form 변수에 담음 , 이미지는 FILES에 있음 , 이미지 따로 넣을거니까 부분을 뺀다
         if post_form.is_valid():                 # form변수(PostForm)이 유효하다면(유효성검사)
-            post_form.save()                     #form을 db에 저장.
+            post = post_form.save()                     #form을 db에 저장. # 게시글은 여기서 끝 !!!!##############
+            for image in request.FILES.getlist('file'):
+                request.FILES['file'] = image
+                image_form = ImageForm(files=request.FILES)
+                if image_form.is_valid():
+                    image = image_form.save(commit=False)
+                    image.post = post
+                    image.save()
             # return render(request, 'posts/list.html') ### 왜 이건 안돼지 ?? ####
             return redirect('posts:list')   # form을 db에 저장 후 index페이지로 리다이렉트.
             
     else:
         post_form = PostForm()                   #GET방식이면(form.html이 실행되지 않으면, 즉 그냥 create를 눌렀을때.), form 변수에 담는다.
+        image_form = ImageForm()
     context = {
-        'post_form':post_form             #dict로 데이터를 넘김
+        'post_form':post_form,             #dict로 데이터를 넘김
+        'image_form':image_form
     }
     return render(request, 'posts/form.html', context)   # 생성폼을 실행.
 
